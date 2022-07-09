f5dc5f09b5d1c58427168dab2ed7176cb3ba4e6b
Gabriel Godoy <gabe.profe@gmail.com>
2022-07-09 11:43:46 -0300

    loading page

--- js/main.js
+++ js/main.js
@@ -86,3 +86,25 @@ function drawHeaderLines(width){
   }
 
 }
+
+
+const loadingPage = document.querySelector('.loading__page');
+const mainPage = document.querySelector('#main')
+
+const wait = (delay = 0) =>
+  new Promise(resolve => setTimeout(resolve, delay));
+
+const setVisible = (elementOrSelector, visible) => 
+  (typeof elementOrSelector === 'string'
+    ? document.querySelector(elementOrSelector)
+    : elementOrSelector
+  ).style.display = visible ? 'block' : 'none';
+
+setVisible(mainPage, false);
+setVisible(loadingPage, true);
+
+document.addEventListener('DOMContentLoaded', () =>
+  wait(1000).then(() => {
+    setVisible(mainPage, true);
+    setVisible(loadingPage, false);
+  }));
